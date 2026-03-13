abd6953a33209638d0fbba363a0a01d1f6a84b65
Kanssup <misavanz@gmail.com>
2026-03-12 22:23:07 -0600

    Eliminando la libreria swipl-prolog. No retorna el trace completo

--- Backend/server.js
+++ Backend/server.js
@@ -4,15 +4,14 @@ const engine = new swipl.Engine();
 (async () => {
     try {
         // Cargar el archivo .pl
-        await engine.call("consult('prolog/program.pl').");
+        await engine.call("consult('program.pl').");
 
         // Ahora puedes hacer consultas
-        const result = await engine.call("abuelo(X, Y).");
+        const result = await engine.call("padre(juan, X)");
         console.log(result); // { X: 'maria' }
 
         // Activar trace si necesitas ver el rastreo
-        await engine.call("trace.");
-        
+        await engine.call("trace, notrace.");
     } catch (err) {
         console.error(err);
     } finally {
