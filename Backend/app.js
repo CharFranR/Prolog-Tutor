dd19472476c30b28f93416a287d546cbd5979b14
Kanssup <misavanz@gmail.com>
2026-03-12 23:52:08 -0600

    update of the variables shown in the trace

--- Backend/app.js
+++ Backend/app.js
@@ -1,5 +1,7 @@
 const { spawn } = require('child_process');
+const { TransformarVariables } = require('./prologParser');
 
+//Inicia el SWI-Prolog
 const swipl = spawn('swipl', ['-q', '--no-tty'], {
     stdio: ['pipe', 'pipe', 'pipe']
 });
@@ -7,30 +9,35 @@ const swipl = spawn('swipl', ['-q', '--no-tty'], {
 let output = '';
 let traceData = '';
 
+//captura la respuesta final
 swipl.stdout.on('data', (data) => {
-    console.log('STDOUT:', data.toString());
     output += data.toString();
 });
-
+// captura el proceso del programa (trace)
 swipl.stderr.on('data', (data) => {
-    console.log('TRACE/STDERR:', data.toString());
     traceData += data.toString();
 });
 
 swipl.on('close', (code) => {
     console.log(`Proceso terminado con código: ${code}`);
+    
+    // Usamos la función importada para limpiar el rastro
+    const traceLimpio = TransformarVariables(traceData);
+    
+    console.log("\n--- TRACE HUMANIZADO ---");
+    console.log(traceLimpio);
 });
 
-// Comandos mejorados: aseguran carga, trace completo y cierre
+// Comandos importantes para hacer la consulta
 const comandos = [
-    "leash(-all).",           // No pausar en ningún puerto
-    "visible(+all).",         // Mostrar todos los eventos del trace
-    "consult('prolog/program.pl').", // Cargar archivo
-    "trace.",                 // Activar trace
-    "abuelo(X, Y).",        // Consulta
-    "notrace.",               // Desactivar trace
-    "halt."                   // Cerrar el proceso explícitamente
+    "leash(-all).", // desactiva el leash para ver todo el proceso de trace
+    "visible(+all).", // hace visibles todos los predicados en el trace
+    "consult('prolog/program.pl').", // carga el programa de prolog
+    "trace.", // activa el trace
+    "abuelo(X, Y).", // realiza la consulta
+    "notrace.", // desactiva el trace para evitar más salidas
+    "halt." // termina la ejecución de SWI-Prolog
 ];
 
-swipl.stdin.write(comandos.join('\n'));   
-swipl.stdin.end(); // Cerrar stdin para indicar que no hay más comandos
+swipl.stdin.write(comandos.join('\n')); // enviar los comandos a SWI-Prolog  
+swipl.stdin.end(); // indica que no se enviarán más datos a la entrada estándar
